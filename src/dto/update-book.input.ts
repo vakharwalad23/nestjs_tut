3a82dea56d66528578b9dc72f161f42c60de187f
vakharwalad23 <vakharwala.d@gmail.com>
2023-08-20 21:54:46 +0530

    update fix

--- src/dto/update-book.input.ts
+++ src/dto/update-book.input.ts
@@ -1,8 +1,21 @@
 import { CreateBookInput } from './create-book.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { CreateCategoryInput } from './create-category.input';
 
 @InputType()
-export class UpdateBookInput extends PartialType(CreateBookInput) {
+export class UpdateBookInput {
   @Field(() => Int)
   id: number;
+
+  @Field({ nullable: true })
+  ISBN: string;
+
+  @Field({ nullable: true })
+  title: string;
+
+  @Field(() => Int, { nullable: true })
+  price: number;
+
+  @Field(() => CreateCategoryInput, { nullable: true })
+  category: CreateCategoryInput;
 }
